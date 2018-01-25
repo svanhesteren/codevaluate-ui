a0acbfc6b404bceb2cf5c8987214cfad4dc1d119
svanhesteren <s.vanhesteren@gmail.com>
2018-01-25 18:51:54 +0100

    Evaluation styling

--- src/components/evaluations/EvaluationItem.js
+++ src/components/evaluations/EvaluationItem.js
@@ -3,16 +3,54 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
+import Paper from 'material-ui/Paper'
+
+const style2 = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  // flex: 1,
+  width: 200,
+  height: 200,
+  margin: '10px 10px',
+  padding: 20
+}
+
+const YELLOW = '#ff9933'
+const RED = '#990000'
+const GREEN = '#33cc33'
 
 
 class EvaluationItem extends PureComponent {
 
+
   render() {
+
+
+    const code = this.props.code
+    var codeColor = ""
+    switch(code) {
+      case "Green":
+        codeColor = GREEN
+        break
+      case "Yellow":
+        codeColor = YELLOW
+        break
+      case "Red":
+        codeColor = RED
+        break
+      default:
+        codeColor = "#ffffff"
+    }
+
+
     return (
-      <div>
-        <div>
-            <pre>{JSON.stringify(this.props, null, 2)}</pre>
-        </div>
+      <div style={style2}>
+        
+        <Paper style={{padding: 30, width: 200, 'background-color': `${codeColor}`}}>
+        <h5>Date: {this.props.date}</h5>
+        <pre>{this.props.remark}</pre>
+
+        </Paper>
       </div>
     )
 
